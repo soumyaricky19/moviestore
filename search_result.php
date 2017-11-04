f4fe3d01bc2c535579b217995188b308314b29db
dhawal1003 <ddp160330@utdallas.edu>
2017-11-04 16:51:58 -0500

    Folders updated in code

--- search_result.php
+++ search_result.php
@@ -5,10 +5,11 @@
         <script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", rel="stylesheet">
         <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
-        <script src="recent.js"></script>
-        <script src="popular.js"></script> 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="style.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/recent.js"></script>
+        <script src="js/popular.js"></script> 
+        <script src="js/add_cart.js"></script>
     </head>
     <body> 
         <nav class='navbar navbar-inverse' style='background-color: rgba(10, 10, 10, 1); margin:0%;'>
