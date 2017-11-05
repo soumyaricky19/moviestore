81da7a1a843cc1da1b99d96ebd797b49ac4fa57c
Soumya Mishra <soumya.ricky19@gmail.com>
2017-11-05 01:40:53 -0600

    updated description

--- description.php
+++ description.php
@@ -18,7 +18,8 @@
                     <li class='active'><a href='home.html'>Home</a></li> 
                     <li><a href="">Login</a></li>
                     <li><a href='signup.html'>Sign Up</a></li>
-                    <li><a href='cart.html'>Cart</a></li>
+                    <li><a href='cart.php'>Cart</a></li>
+                    <li><a href='order_history.php'>Orders</a></li>
                 </ul>
                 <form method = 'GET' action = 'search_result.php' class='navbar-form navbar-right'>
                     <div class='form-group'>
