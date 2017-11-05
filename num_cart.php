45395a8d60f571759145bc8a8156c9608b493a4c
Soumya Mishra <soumya.ricky19@gmail.com>
2017-11-05 01:01:04 -0600

    cart number

---
+++ num_cart.php
@@ -0,0 +1,28 @@
+ <?php
+    session_start();
+    if(!isset($_SESSION["user_id"])) { 
+        header('Location: home.html');
+        exit();
+    }
+    $user_id = $_SESSION["user_id"];
+    // $user_id ="soumyaricky19";
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $db = "onlinemoviestore";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    $cart_query="select * from cart where user_id='".$user_id."'";
+    $cart_result=mysqli_query($conn, $cart_query);
+    $count=0;
+    while ($cart_row = mysqli_fetch_array($cart_result))
+    {     
+        $count+=1;
+    }
+    echo $count;
+?>
