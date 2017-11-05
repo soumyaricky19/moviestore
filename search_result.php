2b4fd13a2567cb048480cedbc29538b2e3cfb469
Soumya Mishra <soumya.ricky19@gmail.com>
2017-11-04 22:05:56 -0600

    genre search

--- search_result.php
+++ search_result.php
@@ -40,6 +40,7 @@
                     }
                     $stop_list=array(",",";",":","a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves");
                     $search_text = $_GET["search"];
+                    $genre_id = $_GET["genre"];
                     $servername = "localhost";
                     $username = "root";
                     $password = "root";
@@ -50,23 +51,28 @@
                     if (!$conn) {
                         die("Connection failed: " . mysqli_connect_error());
                     }
-                    $tokens=tokenize($search_text);
-                    
-                    foreach($tokens as $token) {
-                        if (!in_array($token,$stop_list))
-                        {
-                            //Title search 
-                            $query=$query."select * from movie where is_available=1 and title like '%".$token."%'"." union ";
+                    if ($search_text != "" || $genre == "")
+                        $tokens=tokenize($search_text);
+                        
+                        foreach($tokens as $token) {
+                            if (!in_array($token,$stop_list))
+                            {
+                                //Title search 
+                                $query=$query."select * from movie where is_available=1 and title like '%".$token."%'"." union ";
 
-                            //Director search
-                            $query=$query."select * from movie where is_available=1 and director like '%".$token."%'"." union ";
-                            
-                            //Actor search
-                            $query=$query."select * from movie where is_available=1 and movie_id in (select movie_id from movie_actor where actor_id in (select actor_id from actors where actor_name like '%".$token."%'))"." union ";
+                                //Director search
+                                $query=$query."select * from movie where is_available=1 and director like '%".$token."%'"." union ";
+                                
+                                //Actor search
+                                $query=$query."select * from movie where is_available=1 and movie_id in (select movie_id from movie_actor where actor_id in (select actor_id from actors where actor_name like '%".$token."%'))"." union ";
+                            }
                         }
+                        // echo $query;
+                        $query=$query."select * from movie where 0=1";
+                    else
+                    {
+                        $query="select * from movie where is_available=1 and movie_id in (select movie_id from movie_genre where genre_id=".$genre_id.")";
                     }
-                    // echo $query;
-                    $query=$query."select * from movie where 0=1";
                     display($conn,$query);
                     
                     function display($conn,$query) {
